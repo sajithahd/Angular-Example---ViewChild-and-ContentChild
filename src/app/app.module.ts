0e3a6d761e7e796a128b2224219717af537d2354
sajithahd <hdsajitha@gmail.com>
2021-01-08 17:05:12 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ParentComponent } from './parent.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, ParentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
