75dcce6ec77823aefc31d43c12a927c326a84b96
sajithahd <hdsajitha@gmail.com>
2021-01-08 20:10:53 +0530

    2 files modified

--- src/app/child-list.component.ts
+++ src/app/child-list.component.ts
@@ -1,11 +1,24 @@
+import { style } from "@angular/animations";
 import { Component, Input } from "@angular/core";
 import { ListItem } from "./list-item";
 
 @Component({
   selector: "child-list",
   template: `
-    <div *ngFor="let item of items; let i = index">{{ item.name }}</div>
-  `
+    <div
+      *ngFor="let item of items; let i = index"
+      [ngClass]="{ selected: item.display }"
+    >
+      {{ item.name }}
+    </div>
+  `,
+  styles: [
+    `
+      .selected {
+        background: red;
+      }
+    `
+  ]
 })
 export class ChildListComponent {
   @Input() items: ListItem[];
--- src/app/parent.component.ts
+++ src/app/parent.component.ts
@@ -13,7 +13,7 @@ export class ParentComponent {
 
   constructor() {
     this.items = [
-      { name: "list item 1", display: false },
+      { name: "list item 1", display: true },
       { name: "list item 2", display: false },
       { name: "list item 3", display: false },
       { name: "list item 4", display: false }    
