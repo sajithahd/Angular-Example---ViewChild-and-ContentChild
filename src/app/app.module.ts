773d3bfa7ef5f9c468b36a8a1a211ec03fa24e3b
sajithahd <hdsajitha@gmail.com>
2021-01-08 17:08:56 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent.component';
+import { ChildListComponent } from './child-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ParentComponent ],
+  declarations: [ AppComponent, ParentComponent, ChildListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/child-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+
+  selector: 'child-list',
+  template:`
+    
+  `
+})
+
+
+export class ChildListComponent{
+
+}
