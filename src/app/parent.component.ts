9008246e846bf9c8094306cc043231a317462f2c
sajithahd <hdsajitha@gmail.com>
2021-01-08 18:23:22 +0530

    1 file modified

--- src/app/parent.component.ts
+++ src/app/parent.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { ListItem } from "./list-item";
 
-
-
 @Component({
   selector: "parent",
   template: `
@@ -12,4 +10,13 @@ import { ListItem } from "./list-item";
 })
 export class ParentComponent {
   items: ListItem[];
+
+  constructor() {
+    this.items = [
+      { name: "list item 1", display: false },
+      { name: "list item 2", display: false },
+      { name: "list item 3", display: false },
+      { name: "list item 4", display: false }    
+    ];
+  }
 }
